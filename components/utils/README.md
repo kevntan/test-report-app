06bfebf2f1e8009d3dd223c884c46f5da48704b0
drigoalexander <drigosihombinga@gmail.com>
2024-06-18 10:05:17 +0700

    boilerplate nuxt 3

---
+++ components/utils/README.md
@@ -0,0 +1,3 @@
+# Utils Component
+
+Here in Utils, store your atomic component in here, e.g : card, icon
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "shadcn-nuxt",
+    "formkit/auto-animate/nuxt",
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
